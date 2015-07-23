c6ca871e83a9b0dc710248438ed7732b3850026b
kbharti <kbharti@saymedia.com>
2015-07-23 12:36:07 -0700

    fixed socket events

--- public/app.js
+++ public/app.js
@@ -3,10 +3,45 @@ var socket = io();
 
 chatapp.controller('chatController', function($scope){
     
-    $scope.sendMessage = function () {
-        
-        socket.emit('chat_message', message);
-        
+    $scope.msgHist = [];
+    $scope.userlist =[];
+
+    $scope.send = function () {
+    	if($scope.message.substring(0,5) == '/name') {    		
+    		socket.emit('name_change', $scope.message.substring(6,$scope.message.length));
+    		$scope.message = "";
+    		
+    	}
+    	else {	
+	        socket.emit('chat_message', $scope.message);	        
+    		$scope.message = "";	
+    	}    
     };
+
+    socket.on('chat_message', function (obj) {
+    	console.log(obj);
+    	$scope.msgHist.push({"id": obj.id, "msg": obj.message});
+    	$scope.$apply();
+    	
+    });
+
+    socket.on("name_change", function(list) {
+    	$scope.userlist = list;
+    	$scope.$apply();
+    });
+
+    socket.on('user_connected', function (userlist) {
+    	console.log("user connected");
+    	$scope.userlist = userlist;
+    	$scope.$apply();
+    });
+
+    socket.on('user_disconnected', function (obj) {
+    	$scope.userlist = _.remove($scope.userlist, function (n){
+       		return n['id'] == obj.id;
+       	});
+
+       	$scope.$apply();
+    });
     
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app      = express();
 var port = 9000;
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,15 +11,43 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+var userlist = [];
+
 io.on('connection', function(socket) {
-    console.log(socket.id+" has connected");
+	userlist.push({"id":socket.id, "name": undefined});
+	
+	io.emit('user_connected', userlist);
     
-    socket.on('chat_message', function(msg){
-        console.log("message : "+msg); 
+    console.log(socket.id+" has connected.");
+  
+    socket.on('chat_message', function(msg) {
+    	var nameToSend = undefined;
+    	
+    	_.forEach(userlist, function(n) {
+    		if(n['id'] == socket.id)
+    			nameToSend = n['name'] || n['id'];
+    	});
+        io.emit('chat_message', {"id":nameToSend, "message":msg});
     });
     
+    socket.on('name_change', function(name) {
+    	_.forEach(userlist, function(n) {
+    		if(n['id'] == socket.id) {
+    			n['name'] = name;
+    			//io.emit("name_change",{"id":n['id'], "name": n['name']});
+    			io.emit("name_change",userlist);
+    		}
+    	});
+    });
+
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        console.log(socket.id + " has disconnected.");
+
+       	_.remove(userlist, function (n){
+       		return n['id'] == socket.id;
+       	});
+
+       	io.emit('user_disconnected', {"id":socket.id}); 	
     });
 });
 
